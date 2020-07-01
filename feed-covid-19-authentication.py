386acafab8556b032eeaffd768f0382a0abbc020
Aaron Williams <aaron@flex.io>
2020-07-01 14:00:49 -0500

    authenticate users using authentication function

---
+++ feed-covid-19-authentication.py
@@ -0,0 +1,13 @@
+
+# ---
+# name: feed-covid-19-authentication
+# deployed: true
+# title: Covid-19 Authentication
+# description: Authenticates callers accessing the integration
+# ---
+
+def flex_handler(flex):
+
+    # authenticate all registered users
+    pass
+
